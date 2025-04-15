a3fe079931ae8500b4c42bdede386afe5b6cfc51
Jahari <jayjay8309@gmail.com>
2025-04-15 13:12:59 -0400

    added parser

---
+++ FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameParser {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Prompt user
+        System.out.print("Please enter your name: ");
+        String
+
+    }
+}
